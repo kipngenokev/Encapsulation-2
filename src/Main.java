8c699f9409e40085ece7720a7227b29ace6953ae
brian070069 <kkipngeno409@gmail.com>
2024-07-30 15:33:14 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+//        Player player =new Player();
+//        player.name="Kip";
+//        player.health=20;
+//        player.weapon="sword";
+//
+//        int damage =10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health="+player.healthRemaining());
+//
+//        player.loseHealth(11);
+//        System.out.println("remaining health="+player.healthRemaining());
 
+        EnhancedPlayer Kev =new EnhancedPlayer("kev",200,"Sword");
+        System.out.println("Initial health="+Kev.healthRemaining());
     }
 }
